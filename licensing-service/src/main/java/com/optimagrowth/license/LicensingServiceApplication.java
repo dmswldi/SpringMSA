cec9b48c8e4dafdf617eba56809eb2df376d74c3
김은지 <eeeunzz20@mqnic.com>
2023-05-15 10:06:30 +0900

    ADD: config-server

---
+++ licensing-service/src/main/java/com/optimagrowth/license/LicensingServiceApplication.java
@@ -0,0 +1,38 @@
+package com.optimagrowth.license;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+// 이 클래스가 bean을 정의하는 출처라고 스프링 컨테이너에 알림
+// 1. 내부적으로 애플리케이션 클래스를 configuration 클래스로 지정
+// 2. 자바 클래스 경로에 있는 모든 클래스를 자동으로 스캔
+@SpringBootApplication
+public class LicensingServiceApplication {
+
+    public static void main(String[] args) {
+        // ApplicationContext 객체를 반환
+        SpringApplication.run(LicensingServiceApplication.class, args);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);// 기본 로케일 설정
+        return localeResolver;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setUseCodeAsDefaultMessage(true);// 메시지가 발견되지 않아도 에러를 던지지 않고 메시지 코드를 반환
+        messageSource.setBasenames("messages");// 언어 프로퍼티 파일의 기본 이름을 설정
+        return messageSource;
+    }
+
+}
